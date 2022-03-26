6bcf6fc269b72140e86cef7ee0a6a024057c4557
VisionBao <vision8888888@gmail.com>
2022-03-26 18:23:43 +0800

    init

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/user')
+async def user(
+        *,
+        user_id: int = Query(..., title="The ID of the user to get", gt=0)
+):
+    return {'user_id': user_id}
