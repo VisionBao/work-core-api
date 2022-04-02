0c4715c9feb50647a49263fe9609abbb45a2002b
VisionBao <vision8888888@gmail.com>
2022-04-03 02:25:36 +0800

    fix language api

--- api/api_v1/endpoints/languages.py
+++ api/api_v1/endpoints/languages.py
@@ -12,3 +12,20 @@ router = APIRouter()
 def get_languages(db: Session = Depends(get_db)):
     db_languages = crud.get_languages(db)
     return db_languages
+
+
+@router.get('/language', response_model=schemas.Language)
+def get_languages(lang_id: int = None, lang_code: str = None, db: Session = Depends(get_db)):
+    db_language = None
+    if lang_id:
+        db_language = crud.get_language_by_id(db, lang_id)
+
+    if lang_code:
+        db_language = crud.get_language_by_code(db, lang_code)
+
+    if db_language is None:
+        raise HTTPException(status_code=404, detail="error")
+    else:
+        return db_language
+
+
--- crud/crud_language.py
+++ crud/crud_language.py
@@ -7,5 +7,9 @@ def get_languages(db: Session):
     return db.query(models.Language).all()
 
 
-def get_language(db: Session, lang_id: int):
+def get_language_by_id(db: Session, lang_id: int):
     return db.query(models.Language).filter(models.Language.id == lang_id).first()
+
+
+def get_language_by_code(db: Session, lang_code: str):
+    return db.query(models.Language).filter(models.Language.lang_code == lang_code).first()
